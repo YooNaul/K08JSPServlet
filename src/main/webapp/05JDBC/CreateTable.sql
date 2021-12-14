919e0869c481bd7620307d7ab02c0c82d43ef3af
kgsa0405 <kgsa0405@naver.com>
2021-12-14 17:51:03 +0900

    1

---
+++ src/main/webapp/05JDBC/CreateTable.sql
@@ -0,0 +1,45 @@
+
+/* Drop Tables */
+
+DROP TABLE member CASCADE CONSTRAINTS;
+DROP TABLE board CASCADE CONSTRAINTS;
+
+
+
+
+/* Create Tables */
+
+CREATE TABLE member
+(
+	id varchar2(10) NOT NULL,
+	pass varchar2(10) NOT NULL,
+	name varchar2(30) NOT NULL,
+	regidate date DEFAULT SYSDATE NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE board
+(
+	num number NOT NULL,
+	title varchar2(200) NOT NULL,
+	content varchar2(2000) NOT NULL,
+	id varchar2(10) NOT NULL,
+	postdate date DEFAULT SYSDATE NOT NULL,
+	visitcount number(6),
+	PRIMARY KEY (num)
+);
+
+
+
+
+
+/* Create Foreign Keys */
+
+ALTER TABLE board
+	ADD constraint board_mem_fk
+	FOREIGN KEY (id)
+	REFERENCES member (id)
+;
+
+
+
